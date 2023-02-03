1110d60bd5e808a6148a02b65e591052ae76351c
vomkaraxioned <omkarv@axioned.com>
2023-02-03 19:28:44 +0530

    server created

--- index.js
+++ index.js
@@ -8,19 +8,24 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 // Server path
 
-const dbConnector = (orgName,userName,password,res)=>{
+const dbConnector = (orgName, userName, password, res) => {
 	const url = 'mongodb://localhost:27017/tenet';
-	MongoClient.connect(url,(err,db)=>{
-		if(err) throw err;
-		db.collection('users').find({ $and: [ {username:userName}, {orgname:orgName},{password:password} ] }).toArray((err,data)=>{
-			if(err) throw err;
-			data.length > 0 ? res.json(data) : res.json(null)
+	MongoClient.connect(url, (err, db) => {
+		if (err) res.status(500).send({ message: err });
+		db.collection('users').find({ $and: [{ username: userName }, { orgname: orgName }, { password: password }] }).toArray((err, data) => {
+			try {
+				if (err) throw err
+				if(data.length < 1) throw "Invalid Credentials"
+				res.status(200).json(data)
+			} catch (err) {
+				res.status(404).send({ message: err })
+			}
 		})
 	});
 }
 
 
-app.post('/tenet/login',(req,res)=>{
-	const { orgName,userName,password } = req.body;
-	dbConnector(orgName,userName,password,res);
+app.post('/tenet/login', (req, res) => {
+	const { orgName, userName, password } = req.body;
+	dbConnector(orgName, userName, password, res);
 }).listen(3001);
